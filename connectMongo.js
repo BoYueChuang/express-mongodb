afc5e5bfb1e62c3b9af043ffcf04ccfbf55e5ba8
BoYueChuang <otis29516368@gmail.com>
2023-12-23 12:40:32 +0800

    fix post addstudent

--- connectMongo.js
+++ connectMongo.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
+require("dotenv").config();
 
 const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.MONGODB_CONNECT_URI)
         console.log("Connect to MongoDB successfully")
     } catch (error) {
-        console.log("Connect failed " + error.message )
+        console.log("Connect failed " + error.message)
     }
 }
 
--- index.js
+++ index.js
@@ -1,56 +1,13 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = express();
-
-require("dotenv").config();
-
-app.use(express.json());
-
 const connectDB = require("./connectMongo");
+app.use(express.json());
 
 connectDB();
 
 const BookModel = require("./models/book.model");
 const Student = require("./models/student.model");
-
-
-
-app.get("/api/v1/books", async (req, res) => {
-  const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
-  let page = +req.query?.page;
-
-  if (!page || page <= 0) page = 1;
-
-  const skip = (page - 1) * +limit;
-
-  const query = {};
-
-  if (keyword) query.name = { $regex: keyword, $options: "i" };
-
-  try {
-    const data = await BookModel.find(query)
-      .skip(skip)
-      .limit(+limit)
-      .sort({ [orderBy]: sortBy });
-    const totalItems = await BookModel.countDocuments(query);
-
-    const response = {
-      msg: "Ok",
-      data,
-      totalItems,
-      totalPages: Math.ceil(totalItems / +limit),
-      limit: +limit,
-      currentPage: page,
-    };
-
-    return res.status(200).json(response);
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
-
 
 app.get("/student/getStudent", async (req, res) => {
   try {
@@ -76,22 +33,14 @@ app.post("/student/addStudent", async (req, res) => {
       lastName: req.body.lastName,
       email: req.body.email
     });
-
     const newStudent = await studentObj.save();
-    if (newStudent) {
-      res.status(200).json({
-        message: "Student Added..!!",
-        newStudent
-      });
-    } else (err) => {
-      res.status(500).json({
-        message: "Student not Added..!!",
-        error: err
-      });
-    }
-  } catch (err) {
-    res.json({
-      error: err
+    return res.status(200).json({
+      msg: "Ok",
+      newStudent,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
     });
   }
 });
@@ -117,27 +66,6 @@ app.get("/api/v1/books/:id", async (req, res) => {
   }
 });
 
-app.post("/api/v1/books", async (req, res) => {
-  try {
-    const { name, author, price, description } = req.body;
-    const book = new BookModel({
-      name,
-      author,
-      price,
-      description,
-    });
-    const data = await book.save();
-    deleteKeys('Book')
-    return res.status(200).json({
-      msg: "Ok",
-      data,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
 
 app.put("/api/v1/books/:id", async (req, res) => {
   try {
@@ -180,8 +108,12 @@ app.delete("/api/v1/books/:id", async (req, res) => {
   }
 });
 
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
+
+
+module.exports = app;
