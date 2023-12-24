e96b8c110ce584b12b41629ce25641095993a458
BoYueChuang <otis29516368@gmail.com>
2023-12-24 17:18:02 +0800

    cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 const connectDB = require("./connectMongo");
 app.use(express.json());
+
+
+app.use(cors()); // 啟用 CORS
 
 connectDB();
 
