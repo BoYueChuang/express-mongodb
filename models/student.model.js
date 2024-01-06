2aa7204af90bdd5c7fe61e6e4b4ca19de981f8a7
BoYueChuang <otis29516368@gmail.com>
2024-01-06 14:21:50 +0800

    email no required

--- models/student.model.js
+++ models/student.model.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose
 const studentSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: false },
 }, {
     timestamps: true
 })
